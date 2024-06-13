feb5d277fa2eff735f42737b031205b9cf935d93
Rui Dias <rui.ldias@ceiia.com>
2024-06-13 16:03:59 +0100

    build: add prettier and tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-    theme: {
-        extend: {},
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'var(--primary)',
+        'bridge-in-black': 'var(--bridge-in-black)',
+        'bridge-in-green': 'var(--bridge-in-green)',
+        'bridge-in-white': 'var(--bridge-in-white)',
+        'bridge-in-gray': 'var(--bridge-in-gray)',
+        'bridge-in-black-05-opacity': 'var(--bridge-in-black-05-opacity)',
+        'bridge-in-black-50-opacity': 'var(--bridge-in-black-50-opacity)',
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 }
